22ca1caf6e0901067f2273db3a50883b8a44a83c
Matheus Gomes <matheus01gomes01ferreira2001@gmail.com>
2018-01-04 10:16:47 -0200

    alterando os valores da minha calc. e vendo as condições

--- index.php
+++ index.php
@@ -140,9 +140,9 @@ $email = "tamo@gmail.com"
 
 			//Calculadora
 
-			$value1 = 3;
-			$value2 = 5;
-			$operador = "-";
+			$value1 = 10;
+			$value2 = 3;
+			$operador = "%";
 
 
 			//teste de operação:
@@ -151,6 +151,13 @@ $email = "tamo@gmail.com"
 				echo"Soma = ".($value1 + $value2);
 			}elseif ($operador == "-") {
 				echo "Subtração = ". ($value1 - $value2);
+			}elseif ($operador == "/") {
+				echo "Divisão = ". ($value1 / $value2);
+			}elseif ($operador == "*") {
+				echo "Multiplicação = ". ($value1 * $value2);
+			}else {
+				echo "Operado não existe";
+
 			}
 
 			?>
