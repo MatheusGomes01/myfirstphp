0b67f15fea8a9f1fcbfa33e3b0bfe8338ec07eae
Matheus Gomes <matheus01gomes01ferreira2001@gmail.com>
2018-01-09 11:37:34 -0200

    Aula 12... Array, Keys, e for's...

--- aula10.php
+++ aula10.php
@@ -15,10 +15,39 @@
 
         //echo $y[1];
 
-        $var1 = array(0=>"cat",1=>"dog",3);
+        $var1 = array(0=>"cat",1=>"dog",3=>"red",2=>"blue");
 
-        echo $var1[1];
+        //echo $var1[1];
 
+
+         $var2 = array("color"=>"blue","value"=>43);
+
+        //echo $var2["color"]."<br/>";
+        //echo $var2["value"];
+
+         // Print_R
+
+         #print_r($var1);
+
+         #echo "<br/>";
+  
+         // Var_Dump
+         #var_dump($var1);
+
+         for ($i=0; $i < 4; $i++){
+         	#echo $var1[$i]."<br/>";
+         }
+
+         // error
+
+
+         for ($i=0; $i <2; $i++){
+         	#echo $var2[$i]."<br/>";
+         }
+
+         foreach ($var1 as $key => $value) {
+         	echo $key."=>".$value."<br/>";
+         }
 
         ?>
     </fieldset>
