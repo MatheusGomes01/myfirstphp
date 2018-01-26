677d0b9bcac24a117fd94f50c4187eee2c814ae6
Matheus Gomes <matheus01gomes01ferreira2001@gmail.com>
2018-01-26 14:45:57 -0200

    Aula 19, intermediario, (include_once) para ligar paginas

---
+++ aula19.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<header>
+	<h1>Curso Intermediro</h1>
+	<nav>
+		<ul>
+			<li><a href="#">Home</li>
+			<li><a href="#">Empresa</li>
+			<li><a href="#">Quem Somos</li>
+			<li><a href="#">Contatos</li>
+		</ul>
+	</nav>
+</header>
+<section>
+	<p>Conteudo Index</p>
+</section>
+<footer>
+	<p>Todos os direitos Reservados a PontoCOM</p>
+</footer>
+
+
+</body>
+</html>
