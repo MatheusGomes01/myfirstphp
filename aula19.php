98500a842af638290a6e895584f86b3e97101961
Matheus Gomes <matheus01gomes01ferreira2001@gmail.com>
2018-01-26 15:23:33 -0200

    Aula 19, include_once para ligar paginas

--- aula19.php
+++ aula19.php
@@ -4,23 +4,12 @@
 	<title></title>
 </head>
 <body>
-<header>
-	<h1>Curso Intermediro</h1>
-	<nav>
-		<ul>
-			<li><a href="#">Home</li>
-			<li><a href="#">Empresa</li>
-			<li><a href="#">Quem Somos</li>
-			<li><a href="#">Contatos</li>
-		</ul>
-	</nav>
-</header>
+<? include_once "topo.php" ?>
 <section>
 	<p>Conteudo Index</p>
 </section>
-<footer>
-	<p>Todos os direitos Reservados a PontoCOM</p>
-</footer>
+<? include_once "footer.php" ?><!--Linka Pagina-->
+
 
 
 </body>
